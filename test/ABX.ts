e816c82ae21233e09bd482d36104bccd389d5ad6
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-11-03 20:17:07 +0600

    2 Test case added for ABX

--- test/ABX.ts
+++ test/ABX.ts
@@ -9,11 +9,17 @@ describe('ABX Test Suite', () => {
   async function deployOnceFixture() {
     const [owner, acc1, acc2,...otherAccounts] = await ethers.getSigners();
     const abx: ABX = await new ABX__factory(owner).deploy();
-    return { abx, owner, otherAccounts };
+    return { abx, owner, acc1, acc2, otherAccounts };
   }
 
   it("Check initial amount", async function () {
-    const { abx, owner } = await loadFixture(deployOnceFixture);
+    const { abx, owner, acc1, acc2, otherAccounts } = await loadFixture(deployOnceFixture);
     expect(await abx.balanceOf(owner.address)).to.equal(10000000000000000000000000n);
+  });
+
+  it("Transfer Money", async function () {
+    const { abx, owner, acc1, acc2, otherAccounts } = await loadFixture(deployOnceFixture);
+    await abx.transfer(acc1.address, 100);
+    expect(await abx.balanceOf(acc1.address)).to.equal(100);
   });
 });
