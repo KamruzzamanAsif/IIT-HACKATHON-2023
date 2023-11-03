30f056df57bb5ba717bf54e86606ec8be12305b0
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-11-03 14:13:26 +0600

    Added Royalty in NFT

--- contracts/ArtNFT.sol
+++ contracts/ArtNFT.sol
@@ -5,19 +5,26 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 
-contract ArtNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+
+contract ArtNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ERC721Royalty {
     uint256 private _nextTokenId;
 
     constructor(address initialOwner)
         ERC721("ArTNFT", "ANFT")
         Ownable(initialOwner)
-    {}
+    {
+        _setDefaultRoyalty(initialOwner, 15); // feeNumerator = 15
+
+    }
+
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        _setTokenRoyalty(tokenId, to, 15);
     }
 
     // The following functions are overrides required by Solidity.
@@ -49,7 +56,7 @@ contract ArtNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        override(ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Royalty)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
